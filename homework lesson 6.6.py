3923b920e95d7ff6d666ab3267bac8d3cb88ff87
Maxym Kupinets <mkupinets04@gmail.com>
2025-08-18 17:03:36 +0200

    Update homework lesson 6.6.py

--- homework lesson 6.6.py
+++ homework lesson 6.6.py
@@ -4,5 +4,5 @@ df = pd.read_csv("movies_dataset.csv")
 
 filter_df = df[df["Year"] == 2010]
 
-sorted_df = filter_df.sort_values("Rating")
+sorted_df = filter_df.sort_values("Rating", ascending=False)
 print(sorted_df.head(10))
